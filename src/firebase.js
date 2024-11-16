9467d4c602c0841d9d38522e59fcb82ffbde80d5
DruChill <73948926@elp.edu.pe>
2024-11-16 08:55:06 -0500

    update Firebase configuration to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 const firebaseConfig = {
-  //your API keys of Firebase
+  // Your web app's Firebase configuration
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
