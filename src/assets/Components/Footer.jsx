729723bcfb64ee5f09ab0b07cde466f49778957f
DruChill <73948926@elp.edu.pe>
2024-12-08 10:03:48 -0500

    Update Footer component to include dynamic copyright year and link to developer's GitHub profile

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 function Footer() {
+  const today = new Date();
+
   return (
     <footer className='footer pt-3 mt-4 text-body-secondary border-top d-flex justify-content-between'>
-        <p>Desarrollado por - Andru A.RR</p>
-        <p>&copy; 2024</p>
+        <p>Desarrollado por - <a href="https://github.com/DruChill">Andru A.RR</a></p>
+        <p>&copy;{today.getFullYear()}</p>
   </footer>
   )
 }
