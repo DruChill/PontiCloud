fc1afb50fc8beb0bbee72bffb6b904e7b5f487b9
DruChill <73948926@elp.edu.pe>
2024-12-08 11:22:30 -0500

    Update Footer component to change text from "Desarrollado por" to "Powered by" and enhance link styling

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
 
   return (
     <footer className='footer pt-3 mt-4 text-body-secondary border-top d-flex justify-content-between'>
-        <p>Desarrollado por - <a href="https://github.com/DruChill">Andru A.RR</a></p>
+        <p>Powered by <a className='text-decoration-none text-info' href="https://github.com/DruChill">Andru A.RR</a></p>
         <p>&copy;{today.getFullYear()}</p>
   </footer>
   )
