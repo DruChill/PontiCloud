31fdd415025fad8fd56a4b1d1f1892ebaf4ab66d
DruChill <73948926@elp.edu.pe>
2024-12-11 09:35:56 -0500

    Update Toast component messages and styles; remove old announcement and add sponsorship info

--- src/assets/Components/Toast.jsx
+++ src/assets/Components/Toast.jsx
@@ -7,28 +7,28 @@ function Toast() {
         <div className="toast fade show position-fixed bottom-0 end-0 m-3" role="alert" aria-live="assertive" aria-atomic="true">
             <div className="toast-header">
                 <img src="/vite.svg" className='me-2' alt="icon-alert" />
-                <strong className="me-auto">Comunicado 2</strong>
-                <small>V.16</small>
+                <strong className="me-auto">Comunicado!</strong>
+                <small>V0.4.1</small>
                 <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div className="toast-body">
-                Se eliminaron los anuncios! de la web, ahora puedes disfrutar de una experiencia más limpia y rápida.
+            ¡Anuncios eliminados! Ahora navegas más rápido y limpio. Si te gusta esta mejora, apoya el proyecto con una <a href="/yape.JPG" className='text-info'>donación.</a> ¡Gracias por tu apoyo!
             </div>
         </div>
 
 
-        <div className="toast fade show position-fixed bottom-0 end-0 m-3" role="alert" aria-live="assertive" aria-atomic="true">
+        {/* <div className="toast fade show position-fixed top-50 start-50 translate-middle" role="alert" aria-live="assertive" aria-atomic="true">
             <div className="toast-header">
                 <img src="/vite.svg" className='me-2' alt="icon-alert" />
-                <strong className="me-auto">Comunicado 1</strong>
-                <small>Commits on Nov 9, 2024</small>
+                <strong className="me-auto">Sponsor</strong>
+                <small>10 de Diciembre</small>
                 <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div className="toast-body">
-            ¡La web estuvo temporalmente fuera de servicio, pero ya está todo solucionado! Lamentamos los inconvenientes causados y agradecemos tu paciencia. ¡Gracias por seguir con nosotros!
+                <img src="/sponsor.png" alt="img" style={{width: "100%"}} />
             </div>
         </div>
-        
+         */}
     </div>
   )
 }
