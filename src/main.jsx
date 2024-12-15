299f03dfa49a536e02c73d8eb6fadcdf613f8ebc
DruChill <73948926@elp.edu.pe>
2024-12-15 17:17:26 -0500

    Add Search page route to main application

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App.jsx';
 import Contact from './assets/Pages/Contact.jsx'; // Asegúrate de crear este componente
+import Search from './assets/Pages/Search.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </Router>
   </React.StrictMode>,
