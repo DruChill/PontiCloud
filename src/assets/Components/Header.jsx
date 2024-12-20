c7da355d956aafa9acaa69679b1d02423a636ded
DruChill <73948926@elp.edu.pe>
2024-12-20 10:42:41 -0500

    Enhance Header component with dynamic active link highlighting and improved navigation structure

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 function Header() {
+  const location = useLocation();
+
   return (
     <header>
-        <img src="/icon_logo_ligth_mode.png" alt="icon-logo-web-site" />
-        <nav>
+      <img src="/icon_logo_ligth_mode.png" alt="icon-logo-web-site" />
+      <nav>
         <ul>
-            <li>
-              <a href="/" className='Active'>Home</a>
-            </li>
-            {/* <li>
-              <a href="#">Mis Archivos</a>
-            </li> */}
-            <li>
-              <a href="/yape.JPG">Donar</a>
-            </li>
-            <li>
-              <a href="mailto:andruvrr@gmail.com">Contacto</a>
-            </li>
-            {/* <li>
-              <a href="#">Planes</a>
-            </li> */}
+          <li>
+            <a href="/" className={location.pathname === '/' ? 'Active' : ''}>Home</a>
+          </li>
+          <li>
+            <a href="/mis-archivos" className={location.pathname === '/mis-archivos' ? 'Active' : ''}>Mis Archivos</a>
+          </li>
+          <li>
+            <a href="/yape.JPG" className={location.pathname === '/yape.JPG' ? 'Active' : ''}>Donar</a>
+          </li>
+          <li>
+            <a href="mailto:andruvrr@gmail.com" className={location.pathname === '/contacto' ? 'Active' : ''}>Contacto</a>
+          </li>
+          {/* <li>
+            <a href="#">Planes</a>
+          </li> */}
         </ul>
-        </nav>
-        <div className="badge">Nuevo Diseño <i class="bi bi-vector-pen"></i></div>
+      </nav>
+      <div className="badge">Nuevo Diseño <i className="bi bi-vector-pen"></i></div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
