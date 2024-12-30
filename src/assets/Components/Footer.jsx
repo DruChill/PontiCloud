439c61e99e93f988559842d409ad13e766cd05bd
DruChill <73948926@elp.edu.pe>
2024-12-29 21:35:52 -0500

    Fix HTML className attributes and update footer copyright formatting

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -5,19 +5,19 @@ function Footer() {
 
   return (
     <footer>
-    <p>&copy; by AndruRR</p>
+    <p>&copy;by Andru RR</p>
     <div className='social__media'>
       <a href="https://github.com/DruChill/PontiCloud" target='blank'>
-        <i class="bi bi-github"></i>
+        <i className="bi bi-github"></i>
       </a>
       <a href="https://www.paypal.com/donate/?hosted_button_id=7P22SAK9FTTJC" target='blank'>
-        <i class="bi bi-paypal"></i>
+        <i className="bi bi-paypal"></i>
       </a>
       <a href="#">
-        <i class="bi bi-twitter-x"></i>
+        <i className="bi bi-twitter-x"></i>
       </a>
       <a href="#">
-        <i class="bi bi-discord"></i>
+        <i className="bi bi-discord"></i>
       </a>
     </div>
     <p>&hearts; {today.getFullYear()}</p>
