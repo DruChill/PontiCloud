93dd1a65ca645642942e60602d02c12d5181bf3c
DruChill <73948926@elp.edu.pe>
2025-02-04 16:20:49 -0500

    Refactor Header component layout and improve accessibility; integrate theme selector

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import ThemeSelector from './ThemeSelector';
 
@@ -6,17 +6,28 @@ function Header() {
   const location = useLocation();
 
   return (
-    <header>
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
+    <header className='font-semibold text-base-content py-5 items-center bg-base-200'>
+      <div className="flex justify-around items-center">
+        <div className=''>
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16" />
               </svg>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li><a>Item 1</a></li>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              <li><a>phone</a></li>
               <li>
                 <a>Parent</a>
                 <ul className="p-2">
@@ -27,27 +38,27 @@ function Header() {
               <li><a>Item 3</a></li>
             </ul>
           </div>
-          <img className='w-20' src="/icon_logo_ligth_mode.png" alt="logo-site" />
+          <a className="text-xl">PontiCloud</a>
         </div>
-        <div className="navbar-center hidden lg:flex">
+
+
+        <div className="hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
+              <a href="/">Home</a>
             </li>
-            <li><a>Item 3</a></li>
+            <li>
+              <a href="#">Yape</a>
+            </li>
+            <li>
+              <a href="#">Contacto</a>
+            </li>
           </ul>
         </div>
-        <div className="navbar-end">
-          <ThemeSelector />
-        </div>
+      <div className="">
+        <ThemeSelector />
       </div>
+    </div>
     </header>
   );
 }
