94d234fe241dd37ccb9c774e9b7759046c1134be
DruChill <73948926@elp.edu.pe>
2025-02-04 11:54:42 -0500

    Update React and Vite dependencies; add Tailwind CSS and DaisyUI configuration

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const { default: daisyui } = require('daisyui');
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"]
+  },
+
+};
