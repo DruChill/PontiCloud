c5d6898df6840a2180f27a6c5f9953edd746a580
DruChill <73948926@elp.edu.pe>
2025-02-10 18:17:15 -0500

    Comment out unused icon in ReusableSection component for cleaner presentation

--- src/assets/Components/ReusableSection.jsx
+++ src/assets/Components/ReusableSection.jsx
@@ -6,7 +6,7 @@ const ReusableSection = ({ title, highlight, paragraph }) => {
       <h1 className='font-bold text-6xl'>
         {title}<span className='text-primary'>{highlight}</span>
       </h1>
-      <i className="bi bi-option"></i> Text Animation
+      {/* <i className="bi bi-option"></i> Text Animation */}
       <p className='my-4 max-w-xl text-base sm:text-lg leading-relaxed'>{paragraph}</p>
     </div>
   );
