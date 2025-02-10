cbfdc712fc23fdff0e982097088f60839c52ecc7
DruChill <73948926@elp.edu.pe>
2025-02-10 17:13:02 -0500

    Refactor Yape component to enhance layout with ReusableSection and add modal functionality

--- src/assets/Pages/Yape.jsx
+++ src/assets/Pages/Yape.jsx
@@ -1,31 +1,89 @@
 import React from 'react'
 import Header from '../Components/Header'
 import Footer from '../Components/Footer'
-
+import ReusableSection from '../Components/ReusableSection'
 
 function Yape() {
   return (
-    <div>
+    <div className='hero__container'>
       <Header />
-      <main>
-        <img src="/yape.JPG" alt=""  style={{ width: '26%', borderRadius: '16px' }} />
+      <div className="lg:p-0 p-3 grid grid-cols-1 lg:grid-cols-2 items-center text-base-content gap-4 lg:container mx-auto">
         <div>
-            <h1>Yape - Plin</h1>
-            <p>Brindarte el mejor contenido posible de manera gratuita no es una tarea fácil. Tu apoyo nos ayudará a seguir mejorando y manteniendo el sitio web.</p>
-
-            <div className='Card'>
-                <p>Colaboradores:</p>
-                <ul>
-                    <li>
-                        <img src="/vite.svg" alt="" />
-                        <h5>Andru Ramirez R</h5>
-                        <span>Yape: S/5,00</span>
-                    </li>
-                </ul>
+          <ReusableSection
+            title='¡Yape!'
+            highlight='Tu archivo'
+            paragraph='¡Sube tu archivo aquí! Solo usuarios premium pueden subir archivos.'
+          />
+          {/* Open the modal using document.getElementById('ID').showModal() method */}
+          <button className="btn" onClick={()=>document.getElementById('my_modal_2').showModal()}>open modal</button>
+          <dialog id="my_modal_2" className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">Press ESC key or click outside to close</p>
             </div>
+            <form method="dialog" className="modal-backdrop">
+              <button>close</button>
+            </form>
+          </dialog>
         </div>
 
-      </main>
+        <div>
+          <div className="stats shadow lg:stats-horizontal stats-vertical">
+            <div className="stat">
+              <div className="stat-figure text-primary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-8 w-8 stroke-current">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+                </svg>
+              </div>
+              <div className="stat-title">Total Likes</div>
+              <div className="stat-value text-primary">25.6K</div>
+              <div className="stat-desc">21% more than last month</div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-8 w-8 stroke-current">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 10V3L4 14h7v7l9-11h-7z"></path>
+                </svg>
+              </div>
+              <div className="stat-title">Page Views</div>
+              <div className="stat-value text-secondary">2.6M</div>
+              <div className="stat-desc">21% more than last month</div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <div className="avatar online">
+                  <div className="w-16 rounded-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                  </div>
+                </div>
+              </div>
+              <div className="stat-value">86%</div>
+              <div className="stat-title">Tasks done</div>
+              <div className="stat-desc text-secondary">31 tasks remaining</div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
       <Footer />
     </div>
   )
