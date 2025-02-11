1216755433931a2ee533bcd23fb25ae0e3efce1d
DruChill <73948926@elp.edu.pe>
2025-02-11 12:09:01 -0500

    Remove React.StrictMode to prevent issues with NProgress and visits in routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import Yape from './assets/Pages/Yape.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Aquí se definen las rutas de la aplicación con React Router
+  // Quitar React.StrictMode para evitar problemas con NProgress y visitas
   <React.StrictMode>
     <Router>
       <Routes>
@@ -17,5 +19,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/yape" element={<Yape />} />
       </Routes>
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
