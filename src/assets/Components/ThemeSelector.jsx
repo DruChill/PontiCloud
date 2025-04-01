53d68ce1eeec3d8d93f46b86bd072de4655b1f79
DruChill <73948926@elp.edu.pe>
2025-04-01 08:31:28 -0500

    Remove duplicate themes from ThemeSelector component

--- src/assets/Components/ThemeSelector.jsx
+++ src/assets/Components/ThemeSelector.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 const themes = [
-  "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave",
-  "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua",
+  "light", "dark", "cupcake", "bumblebee", "emerald", "corporate",
+  "retro", "valentine", "halloween", "garden", "forest", "aqua",
   "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk",
   "autumn", "business", "acid", "lemonade", "night", "coffee", "winter", "dim",
   "nord", "sunset"
