cd768160aa6d8b92df47e754541b5c417607ccea
DruChill <73948926@elp.edu.pe>
2025-04-02 09:14:33 -0500

    Update footer links to include Instagram profile

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
 
   return (
     <footer className='flex justify-around items-center text-md p-3 lg:p-5 bg-base-100 text-base-content'>
-    <p>Powered by: <a href="#" className='link'>Dru</a></p>
+    <p>Powered by: <a href="https://www.instagram.com/andrurr_7u7/" target='_blank' className='link'>Dru</a></p>
     <div className='flex text-2xl gap-4'>
       <a href="https://github.com/DruChill/PontiCloud" target='blank'>
         <i className="bi bi-github"></i>
@@ -19,10 +19,10 @@ function Footer() {
       </a>
       {/* <a href="#">
         <i className="bi bi-discord"></i>
-      </a>
-      <a href="#">
-        <i className="bi bi-book"></i>
       </a> */}
+      <a href="https://www.instagram.com/andrurr_7u7/" target='blank'>
+        <i className="bi bi-instagram"></i>
+      </a>
     </div>
     <p>&hearts; {today.getFullYear()}</p>
   </footer>
