4a9d4d74175cb8e6900e02d3c1cc3d04ff643c65
druchill <73948926@elp.edu.pe>
2025-05-27 09:03:34 -0500

    Refactor ReusableSection component: enhance layout with max-width and improve text wrapping for better readability

--- src/assets/Components/ReusableSection.jsx
+++ src/assets/Components/ReusableSection.jsx
@@ -1,11 +1,11 @@
 const ReusableSection = ({ title, highlight, paragraph }) => {
   return (
-    <div>
-      <h1 className='font-bold text-6xl'>
+    <div className="max-w-xl">
+      <h1 className='font-bold text-6xl text-wrap'>
         {title}<span className='text-primary'>{highlight}</span>
       </h1>
       {/* <i className="bi bi-option"></i> Text Animation */}
-      <p className='my-4 max-w-xl text-base sm:text-lg leading-relaxed'>{paragraph}</p>
+      <p className='my-4 text-base text-wrap sm:text-lg leading-relaxed'>{paragraph}</p>
     </div>
   );
 };
